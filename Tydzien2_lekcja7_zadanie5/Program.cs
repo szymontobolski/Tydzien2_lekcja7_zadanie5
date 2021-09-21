a273faa5b122f7b1a7b88ff154e40af89c943921
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-21 20:36:37 +0200

    Add project files.

---
+++ Tydzien2_lekcja7_zadanie5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tydzien2_lekcja7_zadanie5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
